87042c11a90380deb66869fbc1cb31718406d482
Christian Sloper <christian.sloper@gmail.com>
2023-04-08 23:20:06 +0200

    trying to keep answers between submits

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def get_questions():
     random.seed(date.today().toordinal())
 
     questions = []
-    for _ in range(20):
+    for _ in range(2):
         a = random.randint(2, 20)
         b = random.randint(2, 20)
         q = f"{a} * {b}"
@@ -18,11 +18,24 @@ def get_questions():
     random.shuffle(questions)  # Shuffle the list of questions
     return questions
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # Define a list of 20 math questions
 
-    return render_template('index.html', questions= get_questions())
+    if request.method == 'POST':
+        # Check if all answers are correct
+        answers = [request.form[f'answer{i}'] for i in range(1, 21)]
+        correct_answers = [str(q['a']) for q in get_questions()]
+        if answers != correct_answers:
+            flash('Sorry, not all answers are correct. Please try again.', 'danger')
+            return render_template('index.html', questions= get_questions(), answers=request.form)
+
+        # Calculate score and redirect to success page
+        score = calculate_score(answers)
+        return redirect(url_for('success', score=score))
+
+
+    return render_template('index.html', questions= get_questions(), answers=request.form)
 
 
 def calculate_score(answers):
@@ -33,19 +46,6 @@ def calculate_score(answers):
         str(get_questions()[i]['a']) == answer.strip()
         for i, answer in enumerate(answers)
     )
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    answers = [request.form[f'answer{i}'] for i in range(1, 21)]
-    # Check if all answers are correct
-    correct_answers = [str(q['a']) for q in get_questions()]
-    if answers != correct_answers:
-        flash('Sorry, not all answers are correct. Please try again.', 'danger')
-        return redirect(url_for('index'))
-
-    # Calculate score and redirect to success page
-    score = calculate_score(answers)
-    return redirect(url_for('success', score=score))
 
 if __name__ == '__main__':
 
