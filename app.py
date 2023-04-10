c82567a0f3e1e3a43bf05c99c732309d03f77582
Christian Sloper <christian.sloper@gmail.com>
2023-04-10 13:32:11 +0200

    turn off autocomplete

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import random
 from datetime import date
 
 app = Flask(__name__)
-app.secret_key = '100digitsofpi'
+app.secret_key = "100digitsofpi"
+
 
 def get_questions():
     """Return a list of 20 questions."""
@@ -14,28 +15,33 @@ def get_questions():
         a = random.randint(2, 20)
         b = random.randint(2, 20)
         q = f"{a} * {b}"
-        questions.append({'q': q, 'a': str(a * b)})
+        questions.append({"q": q, "a": str(a * b)})
     random.shuffle(questions)  # Shuffle the list of questions
     return questions
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     # Define a list of 20 math questions
 
-    if request.method == 'GET':
-        return render_template('index.html', questions= get_questions(), answers=request.form)
-    
-    
+    if request.method == "GET":
+        return render_template(
+            "index.html", questions=get_questions(), answers=request.form
+        )
+
     submitted_answers = list(request.form.values())
-    correct_answers = [str(q['a']) for q in get_questions()]
+    correct_answers = [str(q["a"]) for q in get_questions()]
     if submitted_answers != correct_answers:
-        flash('Sorry, not all answers are correct. Please try again.', 'danger')
-        answers = { k: a if v == a else '' for (k,v),a in zip(request.form.items(), correct_answers)}
-        return render_template('index.html', questions= get_questions(), answers=answers)
+        flash("Sorry, not all answers are correct. Please try again.", "danger")
+        answers = {
+            k: a if v == a else ""
+            for (k, v), a in zip(request.form.items(), correct_answers)
+        }
+        return render_template("index.html", questions=get_questions(), answers=answers)
 
     # Calculate score and redirect to success page
     score = calculate_score(submitted_answers)
-    return redirect(url_for('success', score=score))
+    return redirect(url_for("success", score=score))
 
 
 def calculate_score(answers):
@@ -43,15 +49,15 @@ def calculate_score(answers):
     Calculate the number of correct answers given by the user.
     """
     return sum(
-        str(get_questions()[i]['a']) == answer.strip()
+        str(get_questions()[i]["a"]) == answer.strip()
         for i, answer in enumerate(answers)
     )
 
-@app.route('/success/<int:score>')
+
+@app.route("/success/<int:score>")
 def success(score):
-    return render_template('success.html', score=score)
+    return render_template("success.html", score=score)
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     app.run(debug=True)
-    
