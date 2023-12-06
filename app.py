65fcb28900e26c35c90e1f124e576ce396b56016
Christian Sloper <christian.sloper@gmail.com>
2023-12-06 20:59:52 +0100

    feat: difficult settings

--- app.py
+++ app.py
@@ -1,14 +1,81 @@
 import random
 from datetime import date
 
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 app.secret_key = "100digitsofpi"
 
 
-def get_questions():
+def get_questions(difficulty):
+    if difficulty == "easy":
+        return get_easy_questions()
+    elif difficulty == "medium":
+        return get_medium_questions()
+    elif difficulty == "hard":
+        return get_hard_questions()
+    else:
+        raise ValueError(f"Unknown difficulty: {difficulty}")
+
+
+def get_easy_questions():
     """Return a list of 20 questions."""
+    random.seed(date.today().toordinal())
+
+    questions = []
+    for _ in range(5):
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        q = f"{a} * {b}"
+        questions.append({"q": q, "a": str(a * b)})
+
+    while len(questions) < 9:
+        a = random.randint(500, 2500)
+        b = random.randint(500, 2500)
+        q = f"{a} + {b}"
+        questions.append({"q": q, "a": str(a + b)})
+
+    while len(questions) < 12:
+        a = random.randint(500, 2500)
+        b = random.randint(500, 2500)
+        b, a = sorted([a, b])
+        q = f"{a} - {b}"
+        questions.append({"q": q, "a": str(a - b)})
+
+    random.shuffle(questions)  # Shuffle the list of questions
+    return questions
+
+
+def get_medium_questions():
+    """Return a list of 20 questions."""
+    random.seed(date.today().toordinal())
+
+    questions = []
+    for _ in range(6):
+        a = random.randint(10, 20)
+        b = random.randint(10, 20)
+        q = f"{a} * {b}"
+        questions.append({"q": q, "a": str(a * b)})
+
+    while len(questions) < 11:
+        a = random.randint(1500, 7500)
+        b = random.randint(1500, 7500)
+        q = f"{a} + {b}"
+        questions.append({"q": q, "a": str(a + b)})
+
+    while len(questions) < 14:
+        a = random.randint(1000, 6000)
+        b = random.randint(1000, 6000)
+        b, a = sorted([a, b])
+        q = f"{a} - {b}"
+        questions.append({"q": q, "a": str(a - b)})
+
+    random.shuffle(questions)  # Shuffle the list of questions
+    return questions
+
+
+def get_hard_questions():
+    """Return a list of 16 questions."""
     random.seed(date.today().toordinal())
 
     questions = []
@@ -49,8 +116,20 @@ def index():
             "index.html", questions=get_questions(), answers=request.form, progress=0
         )
 
+    if difficulty := request.form.get("difficulty"):
+        if difficulty == "easy":
+            session["difficulty"] = "easy"
+        elif difficulty == "medium":
+            session["difficulty"] = "medium"
+        elif difficulty == "hard":
+            session["difficulty"] = "hard"
+        else:
+            raise ValueError(f"Unknown difficulty: {difficulty}")
+
+    difficulty = session.get("difficulty", "hard")
+
     submitted_answers = list(request.form.values())
-    correct_answers = [str(q["a"]) for q in get_questions()]
+    correct_answers = [str(q["a"]) for q in get_questions(difficulty)]
     if submitted_answers != correct_answers:
         flash("Sorry, not all answers are correct. Please try again.", "danger")
         answers = {
@@ -63,7 +142,10 @@ def index():
             * 100
         )
         return render_template(
-            "index.html", questions=get_questions(), answers=answers, progress=progress
+            "index.html",
+            questions=get_questions(difficulty),
+            answers=answers,
+            progress=progress,
         )
 
     # Calculate score and redirect to success page
