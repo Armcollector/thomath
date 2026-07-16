409bbb4fb85b75a2d2a4d8451e6bebffad3265f6
Christian Sloper <christian.sloper@gmail.com>
2026-07-16 16:35:16 +0200

    style: ruff changes

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
+"""Main views for app."""
+
 import random
-from datetime import date
+from datetime import UTC, datetime
 
 from flask import Flask, flash, redirect, render_template, request, session, url_for
+from werkzeug.wrappers.response import Response
 
 app = Flask(__name__)
-app.secret_key = "100digitsofpi"
+app.secret_key = "100digitsofpi"  # noqa: S105
+
+NUMBER_OF_HARD_QUESTIONS = 20
+NUMBER_OF_MEDIUM_QUESTIONS = 14
+NUMBER_OF_EASY_QUESTIONS = 12
 
 
-def get_questions(difficulty):
+def get_questions(difficulty: str) -> list[dict[str, str]]:
+    """Return a list of questions based on the difficulty level."""
     if difficulty == "easy":
         return get_easy_questions()
-    elif difficulty == "medium":
+    if difficulty == "medium":
         return get_medium_questions()
-    elif difficulty == "hard":
+    if difficulty == "hard":
         return get_hard_questions()
-    else:
-        raise ValueError(f"Unknown difficulty: {difficulty}")
+
+    difficulty_message = f"Unknown difficulty: {difficulty}"
+    raise ValueError(difficulty_message)
 
 
-def get_easy_questions():
-    """Return a list of 20 questions."""
-    random.seed(date.today().toordinal())
+def get_easy_questions() -> list[dict[str, str]]:
+    """Return a list of NUMBER_OF_EASY_QUESTIONS questions."""
+    random.seed(datetime.now(tz=UTC).date().toordinal())
 
     questions = []
     for _ in range(5):
@@ -29,13 +38,13 @@ def get_easy_questions():
         q = f"{a} * {b}"
         questions.append({"q": q, "a": str(a * b)})
 
-    while len(questions) < 9:
+    for _ in range(4):
         a = random.randint(500, 2500)
         b = random.randint(500, 2500)
         q = f"{a} + {b}"
         questions.append({"q": q, "a": str(a + b)})
 
-    while len(questions) < 12:
+    for _ in range(3):
         a = random.randint(500, 2500)
         b = random.randint(500, 2500)
         b, a = sorted([a, b])
@@ -46,9 +55,9 @@ def get_easy_questions():
     return questions
 
 
-def get_medium_questions():
-    """Return a list of 20 questions."""
-    random.seed(date.today().toordinal())
+def get_medium_questions() -> list[dict[str, str]]:
+    """Return a list of NUMBER_OF_MEDIUM_QUESTIONS questions."""
+    random.seed(datetime.now(tz=UTC).date().toordinal())
 
     questions = []
     for _ in range(6):
@@ -57,13 +66,13 @@ def get_medium_questions():
         q = f"{a} * {b}"
         questions.append({"q": q, "a": str(a * b)})
 
-    while len(questions) < 11:
+    for _ in range(5):
         a = random.randint(1500, 7500)
         b = random.randint(1500, 7500)
         q = f"{a} + {b}"
         questions.append({"q": q, "a": str(a + b)})
 
-    while len(questions) < 14:
+    for _ in range(3):
         a = random.randint(1000, 6000)
         b = random.randint(1000, 6000)
         b, a = sorted([a, b])
@@ -74,9 +83,9 @@ def get_medium_questions():
     return questions
 
 
-def get_hard_questions():
-    """Return a list of 16 questions."""
-    random.seed(date.today().toordinal())
+def get_hard_questions() -> list[dict[str, str]]:
+    """Return a list of NUMBER_OF_HARD_QUESTIONS questions."""
+    random.seed(datetime.now(tz=UTC).date().toordinal())
 
     questions = []
     for _ in range(3):
@@ -91,7 +100,7 @@ def get_hard_questions():
         q = f"Hva er {a} % av {b}"
         questions.append({"q": q, "a": round(a / 100 * b, 2)})
 
-    while len(questions) < 10:
+    for _ in range(2):
         a = random.randint(100, 999)
 
         divisors = [i for i in range(2, 10) if a % i == 0]
@@ -102,14 +111,14 @@ def get_hard_questions():
         q = f"{a} / {b}"
         questions.append({"q": q, "a": str(a // b)})
 
-    while len(questions) < 13:
+    for _ in range(3):
         a = random.randint(1000, 9999)
         b = random.randint(1000, 9999)
         b, a = sorted([a, b])
         q = f"{a} - {b}"
         questions.append({"q": q, "a": str(a - b)})
 
-    while len(questions) < 20:
+    for _ in range(7):
         a = random.randint(2, 10) * random.choice([-1, 1])
         b = random.randint(-100, 100)
         x = random.randint(1, 99) * random.choice([-1, 1])
@@ -121,7 +130,8 @@ def get_hard_questions():
 
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def index() -> str | Response:
+    """Display main page for thomath."""
     if "nr_answered" not in session:
         session["nr_answered"] = 0
     if "difficulty" not in session:
@@ -133,7 +143,8 @@ def index():
         if new_difficulty in ["easy", "medium", "hard"]:
             session["difficulty"] = new_difficulty
         else:
-            raise ValueError(f"Unknown difficulty: {new_difficulty}")
+            message = f"Unknown difficulty: {new_difficulty}"
+            raise ValueError(message)
         return render_template(
             "index.html",
             questions=get_questions(new_difficulty),
@@ -150,10 +161,12 @@ def index():
     if submitted_answers != correct_answers:
         answers = {
             k: "" if (v == "" or float(v) != a) else v
-            for (k, v), a in zip(request.form.items(), correct_answers)
+            for (k, v), a in zip(request.form.items(), correct_answers, strict=False)
         }
         progress = int(
-            sum(a == b for a, b in zip(submitted_answers, correct_answers))
+            sum(
+                a == b for a, b in zip(submitted_answers, correct_answers, strict=False)
+            )
             / len(correct_answers)
             * 100
         )
@@ -176,9 +189,10 @@ def index():
 
 
 @app.route("/success/<int:score>")
-def success(score):
+def success(score: int) -> str:
+    """Display the success page with the user's score."""
     return render_template("success.html", score=score)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
